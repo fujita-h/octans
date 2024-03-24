016ea30ae42051f24cf7769df83da6676c777daa
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-03-24 09:22:21 +0000

    Refactor Azure AD profile photo retrieval in provider

--- lib/auth/providers/azure-ad.ts
+++ lib/auth/providers/azure-ad.ts
@@ -6,15 +6,33 @@ const provider = AzureAD({
   clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
   tenantId: process.env.AZURE_AD_TENANT_ID,
   checks: ['pkce', 'state', 'nonce'],
-  profile: (profile) => ({
-    id: profile.sub,
-    name: profile.name,
-    email: profile.email,
-    image: undefined,
-    uid: nanoid_case_insensitive(16),
-    roles: profile.roles,
-    claims: profile,
-  }),
+  profile: async (profile, tokens) => {
+    // https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples
+    const response = await fetch(`https://graph.microsoft.com/v1.0/me/photos/48x48/$value`, {
+      headers: { Authorization: `Bearer ${tokens.access_token}` },
+    });
+
+    // Confirm that profile photo was returned
+    let image;
+    // TODO: Do this without Buffer
+    if (response.ok && typeof Buffer !== 'undefined') {
+      try {
+        const pictureBuffer = await response.arrayBuffer();
+        const pictureBase64 = Buffer.from(pictureBuffer).toString('base64');
+        image = `data:image/jpeg;base64, ${pictureBase64}`;
+      } catch {}
+    }
+
+    return {
+      id: profile.sub,
+      name: profile.name,
+      email: profile.email,
+      image: image ?? null,
+      uid: nanoid_case_insensitive(16),
+      roles: profile.roles,
+      claims: profile,
+    };
+  },
 });
 
 export default provider;
